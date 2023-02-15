1f42676a3f9e18ecd372f0967788652e5b09f3bc
Herwans <herwansharvel@gmail.com>
2023-02-15 23:25:11 +0100

    init: mariadb configured

---
+++ src/main/java/com/horn/api/HornApiApplication.java
@@ -0,0 +1,13 @@
+package com.horn.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HornApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HornApiApplication.class, args);
+	}
+
+}
